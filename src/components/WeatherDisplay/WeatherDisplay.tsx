f7c12cd47c6f40425fdc521989afabe309900749
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2025-06-10 10:39:21 +0500

    update: ui part of weather display

--- src/components/WeatherDisplay/WeatherDisplay.tsx
+++ src/components/WeatherDisplay/WeatherDisplay.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import dayjs from "dayjs";
 import { useWeatherData } from "@/hooks/useWeatherData";
 import { Skeleton } from "../ui/skeleton";
 import Fade from "../ui/fade";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { WeatherDataSchemaType } from "@/schemas/weather-data";
+import { getTimeZoneInfo } from "@/helper/timezone";
+import tzlookup from "tz-lookup";
 
 function WeatherDisplay() {
   const { state } = useWeatherData();
@@ -22,10 +25,42 @@ const WeatherDisplayContent = ({ data }: { data: WeatherDataSchemaType }) => {
     <Fade>
       <div className="grid sm:grid-cols-3 grid-cols-1 gap-4 mt-2">
         <div className="sm:col-span-1 dark:bg-slate-100/10 bg-slate-100 p-3 rounded-lg min-h-36 border border-gray-400">
-          <h2 className="text-xl font-bold">{data.data.city.name}</h2>
-          <p className="text-sm text-gray-500">{data.data.city.country}</p>
-          <Icon icon="solar:sunrise-broken" width="24" height="24" />
-          <Icon icon="solar:sunset-broken" width="24" height="24" />
+          <h2 className="text-xl text-center font-bold">
+            {data.data.city.name}
+          </h2>
+          <p className="text-sm text-gray-500 text-center">
+            {data.data.city.country}
+          </p>
+          <span className="text-sm text-gray-500 text-center block mt-2">
+            {
+              getTimeZoneInfo(
+                data.data.city.timezone,
+                tzlookup(data.data.city.coord.lat, data.data.city.coord.lon),
+              ).label
+            }
+          </span>
+          <div className="grid grid-cols-2 gap-2 mt-3">
+            <div className="flex items-center gap-2 col-span-2">
+              <Icon icon="iconoir:position" width="24" height="24" />
+              <span>{Object.values(data.data.city.coord).join(", ")}</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <Icon icon="solar:sunrise-broken" width="24" height="24" />{" "}
+              <span>
+                {dayjs((data.data.city.sunrise || 0) * 1000).format("HH:mm")}
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <Icon icon="solar:sunset-broken" width="24" height="24" />
+              <span>
+                {dayjs((data.data.city.sunset || 0) * 1000).format("HH:mm")}
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <Icon icon="formkit:people" width="24" height="24" />
+              <span>{data.data.city.population}</span>
+            </div>
+          </div>
         </div>
         <div className="sm:col-span-2 dark:bg-slate-100/10 bg-slate-100 p-3 rounded-lg min-h-36 border border-gray-400">
           <p className="text-2xl font-semibold">
--- src/helper/timezone.ts
+++ src/helper/timezone.ts
@@ -1,15 +1,21 @@
 import timezones from "@/assets/data/timezones.json";
 import { formatOffset } from "./offset-format";
 
-export function getTimeZoneInfo(offsetInSeconds: number): {
+export function getTimeZoneInfo(
+  offsetInSeconds?: number,
+  utc?: string,
+): {
   name: string;
   label: string;
 } {
+  if (!offsetInSeconds || !utc) return { name: "Unknown", label: "Unknown" };
   const offsetMinutes = offsetInSeconds / 60;
   const offsetInHours = offsetMinutes / 60;
 
+  console.log({ offsetInHours });
+
   for (const tz of timezones) {
-    if (tz.offset === offsetInHours) {
+    if (tz.offset === offsetInHours && tz.utc.includes(utc)) {
       return {
         name: tz.value,
         label: tz.text,
